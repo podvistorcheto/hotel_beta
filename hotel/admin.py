2d5dbd91c3670e0ca834c24091beac313a80c306
podvistorcheto <denev.s.deyan@gmail.com>
2020-10-24 11:19:17 +0000

    06-Models added for the hotel app

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from .models import Room, Reservation
 # Register your models here.
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        'room_type',
+        'size',
+        'price',
+        'capacity',
+        'roomnumber',
+    )
+    readonly_fields = ('pk',)
+
+    ordering = ('roomnumber',)
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'user',
+        'room',
+        'guests',
+        'check_in',
+        'check_out',
+        'specials',
+        )
+    readonly_fields = ('pk', 'date_posted')
+
+    ordering = ('date_posted',)
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Reservation, ReservationAdmin)
--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,53 @@
+from django.urls import reverse
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
+
+
+class Room(models.Model):
+    ROOM_STATUS = ( 
+    ("1", "available"), 
+    ("2", "not available"),    
+    ) 
+
+    ROOM_TYPE = ( 
+    ("1", "APARTMENT"), 
+    ("2", "VILLA BOR"),
+    ("3","VILLA ASPEN"),
+    ("4", "DOUBLE ROOM"), 
+    ("5", "DELUXE SUITE"),
+    ("6","VILLA ELA"),
+    )
+    #type,no_of_rooms,capacity,prices,Hotel
+    room_type = models.CharField(max_length=50, choices=ROOM_TYPE)
+    capacity = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    size = models.IntegerField(null=True)
+    status = models.CharField(choices=ROOM_STATUS, max_length=15)
+    roomnumber = models.IntegerField()
+
+    def __str__(self):
+        return self.room_type
+
+
+class Reservation(models.Model):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    check_in = models.DateTimeField(null=False)
+    check_out = models.DateTimeField(null=False)
+    guests = models.IntegerField(null=False)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    specials = models.TextField(
+        max_length=256, null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    booking_id = models.CharField(max_length=10, default="null")
+
+    def __str__(self):
+        return self.user.room.check_in.check_out
+
+
